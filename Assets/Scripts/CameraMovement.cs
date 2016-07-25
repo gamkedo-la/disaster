7a557031a7046f66330942217c859160699e4e72
Jeremy Kenyon <lanthos@gmail.com>
2016-07-24 17:41:01 -0700

    Main scene created, scripts attached to the avatar ball created for moving around the camera.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float movementSpeed = 800.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Input.GetAxis ("Horizontal") * transform.right * Time.deltaTime * movementSpeed;
+		transform.position += Input.GetAxis ("Vertical") * transform.forward * Time.deltaTime * movementSpeed;
+		float yHere = Terrain.activeTerrain.SampleHeight (transform.position);
+		Vector3 fixedHeight = transform.position;
+		fixedHeight.y = yHere;
+		transform.position = fixedHeight;
+	}
+}
