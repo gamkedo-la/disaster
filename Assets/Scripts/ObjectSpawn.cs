194b2a2dbfcee1fdafb1298501dd021b1fa10c02
Jeremy Kenyon <lanthos@gmail.com>
2016-08-08 20:38:35 -0700

    When throwing the sphere (or anything with the meteor script on it) and if it hits the little people it will knock them flat face up.

--- Assets/Scripts/ObjectSpawn.cs
+++ Assets/Scripts/ObjectSpawn.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class PeopleSpawn : MonoBehaviour {
+public class ObjectSpawn : MonoBehaviour {
 
     public int howManyObjectsToSpawn = 20;
     public float creationRadius = 3.0f;
@@ -17,8 +17,9 @@ public class PeopleSpawn : MonoBehaviour {
            
             Vector2 randOffset = Random.insideUnitCircle * creationRadius;
             Vector3 newSpot = transform.position;
+            Vector3 highY = new Vector3(0.0f, 1.0f, 0.0f);
             newSpot += Vector3.right * randOffset.x + Vector3.forward * randOffset.y;
-            newSpot.y = Terrain.activeTerrain.SampleHeight(newSpot);
+            newSpot.y = highY.y;
             tempGo.transform.position = newSpot;
 
             tempGo.transform.parent = transform;
---
+++ Assets/Scripts/PeopleMover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PeopleMover : MonoBehaviour {
+    public Transform childTransform;
+    bool scared = false;
+    bool knockedOver = false;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.GetComponent<Meteor>() && knockedOver == false) {
+            knockOver();
+        }
+    }
+
+    private void knockOver()
+    {
+        GetComponent<PutOnGround>().enabled = false;
+        GetComponentInChildren<CameraFacingScript>().enabled = false;
+        Vector3 newSpot = transform.position;
+        newSpot.y += 0.0109f;
+        transform.position = newSpot;
+        Quaternion rot = transform.rotation;
+        childTransform.rotation = Quaternion.identity;
+        transform.rotation = rot * Quaternion.Euler(90.0f, 0.0f, 0.0f);
+        knockedOver = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
