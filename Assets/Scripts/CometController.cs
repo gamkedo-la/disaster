c74ebae7491f80d203fb21a1c4d725dca35c564d
Jeremy Kenyon <lanthos@gmail.com>
2016-08-20 16:28:47 -0700

    Comets will randomly spawn and shoot across the sky.
    Right now it happens A LOT.  This is just for demo pruposes and will be
    scaled back a lot once we are closer to release (or after tomorrow
    even).

---
+++ Assets/Scripts/CometController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CometController : MonoBehaviour {
+
+    public Transform[] cometSpawnPoints;
+    public GameObject comet;
+    public Transform playAreaCenter;
+    public float cometSpeed = 13.0f;
+
+
+    IEnumerator CometSpawner() {
+        yield return new WaitForSeconds(Random.Range(2.0f, 8.0f));
+        SpawnComet();
+    }
+
+    void SpawnComet() {
+        GameObject tempComet = (GameObject) GameObject.Instantiate(comet);
+        int rand = Random.Range(0, 3);
+        tempComet.transform.position = cometSpawnPoints[rand].position;
+        Vector3 randLoc = tempComet.transform.position;
+        Vector2 randOffset = Random.insideUnitCircle * 2.0f;
+        randLoc += Vector3.right * randOffset.x + Vector3.forward * randOffset.y;
+        tempComet.transform.position = randLoc;
+        Quaternion rot = cometSpawnPoints[rand].localRotation;
+        tempComet.transform.rotation = rot;
+        Rigidbody rb = tempComet.GetComponent<Rigidbody>();
+        rb.velocity = tempComet.transform.forward * cometSpeed;
+        Destroy(tempComet, 4.0f);
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.M)) {
+            //StartCoroutine(Spawncomet());
+            SpawnComet();
+        }
+        StartCoroutine(CometSpawner());
+	}
+}
