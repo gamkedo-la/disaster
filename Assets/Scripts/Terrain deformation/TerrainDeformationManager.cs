b09e56d32547d93a3774834993878dcca00f5f14
TimWaskett <tim.waskett@gmail.com>
2016-09-05 18:00:11 +0100

    Added all the terrain deformation test stuff.

---
+++ Assets/Scripts/Terrain deformation/TerrainDeformationManager.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Terrain))]
+public class TerrainDeformationManager : MonoBehaviour
+{
+    [SerializeField] float m_erosionRate = 1f;
+    [SerializeField] int m_errosionChunkDivider = 8;
+
+    private Terrain m_terrain;
+    private float[,] m_originalHeights;
+    private int m_width;
+    private int m_height;
+
+
+    void Awake()
+    {
+        m_terrain = GetComponent<Terrain>();
+
+        m_width = m_terrain.terrainData.heightmapWidth;
+        m_height = m_terrain.terrainData.heightmapHeight;
+    }
+
+
+    void Start()
+    {
+        m_originalHeights = m_terrain.terrainData.GetHeights(0, 0, m_width, m_height);
+
+        StartCoroutine(Erode());
+    }
+
+
+    private IEnumerator Erode()
+    {
+        int chunkWidth = (m_width - 1) / m_errosionChunkDivider;
+        int chunkHeight = (m_height - 1) / m_errosionChunkDivider;
+
+        int jBlock = 0;
+        int iBlock = 0;
+
+        while (true)
+        {
+            int width = jBlock == m_errosionChunkDivider - 1 ? chunkWidth + 1 : chunkWidth;
+            int height = iBlock == m_errosionChunkDivider - 1 ? chunkHeight + 1 : chunkHeight;
+
+            int jStart = jBlock * chunkHeight;
+            int iStart = iBlock * chunkWidth;
+
+            var currentHeights = m_terrain.terrainData.GetHeights(jStart, iStart, width, height);
+
+            var newHeights = new float[height, width];
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    float currentHeight = currentHeights[i, j];
+                    float originalHeight = m_originalHeights[i + iStart, j + jStart];
+
+                    newHeights[i, j] = Mathf.Lerp(
+                        currentHeight, 
+                        originalHeight,
+                        Time.deltaTime * m_erosionRate);
+                }
+            }
+
+            m_terrain.terrainData.SetHeights(jStart, iStart, newHeights);
+
+            iBlock++;
+            iBlock = iBlock % m_errosionChunkDivider;
+
+            if (iBlock == 0)
+            {
+                jBlock++;
+                jBlock = jBlock % m_errosionChunkDivider;
+            }
+
+            yield return null;
+        }
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        var terrainDeformer = col.gameObject.GetComponent<ITerrainDeformer>();
+
+        if (terrainDeformer != null)
+        {
+            terrainDeformer.DeformTerrain(m_terrain, col.contacts[0].point);
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        var terrainDeformer = other.gameObject.GetComponent<ITerrainDeformer>();
+
+        if (terrainDeformer != null)
+        {
+            terrainDeformer.DeformTerrain(m_terrain, other.transform.position);
+        }
+    }
+
+
+    private void OnDestroy()
+    {
+        m_terrain.terrainData.SetHeights(0, 0, m_originalHeights);
+    }
+}
---
+++ Assets/Scripts/Terrain deformation/TerrainDeformer.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TerrainDeformer : MonoBehaviour, ITerrainDeformer
+{
+    [SerializeField] float m_radiusWorldUnits = 5f;
+    [SerializeField] float m_heightWorldUnits = 3f;
+    [SerializeField] AnimationCurve m_profile;
+    [SerializeField] float m_deformationDuration = 0f;
+
+    private bool m_deformed = false;
+    private Rigidbody m_rigidbody;
+    private Terrain m_terrain;
+    private int m_xBase;
+    private int m_yBase;
+    private int m_xSize;
+    private int m_ySize;
+
+
+    void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    public void DeformTerrain(Terrain terrain, Vector3 position)
+    {
+        if (m_deformed)
+            return;
+
+        m_deformed = true;
+
+        m_terrain = terrain;
+
+        int heightMapWidth = terrain.terrainData.heightmapWidth;
+        int heightMapHeight = terrain.terrainData.heightmapHeight;
+
+        //print(string.Format("height: {0}, width: {1}", heightMapWidth, heightMapHeight));
+
+        int radius = Mathf.CeilToInt(m_radiusWorldUnits / terrain.terrainData.heightmapScale.x);
+        float height = m_heightWorldUnits / terrain.terrainData.heightmapScale.y;
+
+        // get the normalized position of this game object relative to the terrain
+        Vector3 coord = (position - terrain.gameObject.transform.position);
+        coord.x = coord.x / terrain.terrainData.size.x;
+        coord.y = coord.y / terrain.terrainData.size.y;
+        coord.z = coord.z / terrain.terrainData.size.z;
+
+        // get the position of the terrain heightmap where the collision happened
+        float xPos = coord.x * heightMapWidth;
+        float yPos = coord.z * heightMapHeight;
+
+        //print("xPos: " + xPos + ", yPos: " + yPos);
+
+        int posXInTerrain = (int) xPos;
+        int posYInTerrain = (int) yPos;
+
+        int xMin = Math.Max(0, posXInTerrain - radius);
+        int xMax = Math.Min(heightMapWidth , posXInTerrain + radius);
+        int yMin = Math.Max(0, posYInTerrain - radius);
+        int yMax = Math.Min(heightMapHeight, posYInTerrain + radius);
+
+        //print(string.Format("xMin: {0}, xMax: {1}, yMin: {2}, yMax: {3}", xMin, xMax, yMin, yMax));
+
+        int xMinToCentre = posXInTerrain - xMin;
+        int yMinToCentre = posYInTerrain - yMin;
+
+        m_xSize = xMax - xMin;
+        m_ySize = yMax - yMin;
+
+        m_xBase = xMin;
+        m_yBase = yMin;
+
+        float[,] sampleHeights = new float[m_xSize, m_ySize];
+
+        for (int i = 0; i < m_xSize; i++)
+        {
+            float sampleXPos = i - xMinToCentre + posXInTerrain;
+
+            for (int j = 0; j < m_ySize; j++)
+            {
+                float sampleYPos = j - yMinToCentre + posYInTerrain;
+                float xDiff = sampleXPos - xPos;
+                float yDiff = sampleYPos - yPos;
+                float dist = Mathf.Sqrt(xDiff * xDiff + yDiff * yDiff) * terrain.terrainData.heightmapScale.x / m_radiusWorldUnits;
+                sampleHeights[i, j] = height * m_profile.Evaluate(dist);
+            }
+        }
+
+        if (m_deformationDuration < 0.01f)
+        {
+            var heights = m_terrain.terrainData.GetHeights(m_xBase, m_yBase, m_xSize, m_ySize);
+            terrain.terrainData.SetHeights(m_xBase, m_yBase, AddHeights(heights, sampleHeights, 1f));
+
+            Destroy(gameObject);
+            //m_rigidbody.isKinematic = true;
+        }
+        else
+        {
+            StartCoroutine(AddHeightsIncrementally(sampleHeights));
+        }
+    }
+
+
+    private float[,] AddHeights(float[,] heights, float[,] sampleHeights, float frac)
+    {
+        //print(string.Format("heights: [{0},{1}], sampleHeights: [{2},{3}]", 
+        //    heights.GetLength(0), heights.GetLength(1), sampleHeights.GetLength(0), sampleHeights.GetLength(1)));
+        //print(string.Format("xSize: {0}, ySize: {1}", m_xSize, m_ySize));
+
+        var sumHeights = new float[m_ySize, m_xSize];
+
+        for (int i = 0; i < m_xSize; i++)
+        {
+            for (int j = 0; j < m_ySize; j++)
+            {
+                sumHeights[j, i] = heights[j, i] + frac * sampleHeights[i, j];
+            }
+        }
+
+        return sumHeights;
+    }
+
+
+    private IEnumerator AddHeightsIncrementally(float[,] sampleHeights)
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<Collider>().enabled = false;
+
+        float startTime = Time.time;
+        float duration = 0;
+
+        while (duration <= m_deformationDuration)
+        {
+            duration = Time.time - startTime;
+            float frac = Time.deltaTime / m_deformationDuration;
+
+            var heights = m_terrain.terrainData.GetHeights(m_xBase, m_yBase, m_xSize, m_ySize);
+            m_terrain.terrainData.SetHeights(m_xBase, m_yBase, AddHeights(heights, sampleHeights, frac));
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
