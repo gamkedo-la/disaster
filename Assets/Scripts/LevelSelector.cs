7df7616d59e409bc5d7d08402b6275a07ae66e5c
Jeremy Kenyon <lanthos@gmail.com>
2016-11-27 10:04:41 -0800

    Main Menu framework created

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour {
+
+	public enum levelSelect { None, Level1, Level2 };
+	public levelSelect currentAction = levelSelect.None;
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "RightController" || other.tag == "LeftController") {
+			if (currentAction == levelSelect.Level1) {
+				SceneManager.LoadScene ("Scene2");
+			} else if (currentAction == levelSelect.Level2) {
+				SceneManager.LoadScene ("Level3");
+			}	
+		}
+	}
+}
