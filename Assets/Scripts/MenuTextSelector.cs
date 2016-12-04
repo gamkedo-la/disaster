f4531810ae7de1625a29e3efae784f5aaca55c5c
Jeremy Kenyon <lanthos@gmail.com>
2016-12-04 09:30:03 -0800

    setup credit text and objects for changing between credits and title

---
+++ Assets/Scripts/MenuTextSelector.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuTextSelector : MonoBehaviour {
+
+	public GameObject credits;
+	public GameObject title;
+	public Text credit_text;
+	public Text title_text;
+	bool titleActive;
+
+	// Use this for initialization
+	void Awake () {
+		title.SetActive (false);
+		title_text.enabled = true;
+		credits.SetActive (true);
+		credit_text.enabled = false;
+		titleActive = true;
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "RightController" || other.tag == "LeftController") {
+			if (titleActive) {
+				titleActive = false;
+				title.SetActive (true);
+				title_text.enabled = false;
+				credits.SetActive (false);
+				credit_text.enabled = true;
+			} else {
+				title.SetActive (false);
+				title_text.enabled = true;
+				credits.SetActive (true);
+				credit_text.enabled = false;
+				titleActive = true;
+			}
+		}
+	}
+}
