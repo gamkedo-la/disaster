b693821a65872d049d7633c30672231f8204b86f
Reid Alford <rmalford2007@gmail.com>
2016-12-04 01:32:19 -0700

    Standalone animated hand model - not hooked up
    Animated hand model fbx file. Original blend file for hand model. Added
    animator to drive animations.

---
+++ Assets/Scripts/HandAnimController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandAnimController : MonoBehaviour {
+
+    private Animator anim;
+    public bool grab = false;
+    public bool point = false;
+    public bool click = false;
+    bool doUpdate = false;
+	// Use this for initialization
+	void Start () {
+        //point = true;
+        doUpdate = true;
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (anim != null && doUpdate)
+        {
+            anim.SetBool("point", point);
+            anim.SetBool("grab", grab);
+            
+           // anim.SetBool("click", click);
+            doUpdate = false;
+        }
+	}
+
+    public void SetTrigger(bool state)
+    {
+        grab = state;
+
+        doUpdate = true;
+    }
+
+    public void SetPoint(bool state)
+    {
+        point = state;
+
+        doUpdate = true;
+    }
+}
